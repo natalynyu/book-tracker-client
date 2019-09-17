c0dd17bb607e05157eefc20f3898a887bc9c497c
natalynyu <natalyn.yu@gmail.com>
2019-09-17 01:04:06 -0400

    Render basic test data
    Include test data and render it on the page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
+const testData = {
+  books: [
+    {title: "Harry Potter and the Sorcerer's Stone", author: "J. K. Rowling",
+      completionDate: "2019-09-10"},
+    {title: "Harry Potter and the Chamber of Secrets", author: "J. K. Rowling",
+      completionDate: "2019-09-12"}
+  ]
 }
 
-export default App;
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      data: testData
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Book Tracker</h1>
+        <ul style={{textAlign: "left"}}>{this.state.data.books.map(book => {
+          return <li key={book.title}>{book.title}</li>
+        })}</ul>
+      </div>
+    )
+  }
+}
+
+export default App
